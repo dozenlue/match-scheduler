9ce44bdfe28e80d838f5a0111befa24a23a1f9cf
Lu Shuang <lushuang@foundersc.com>
2017-12-22 16:41:22 +0800

    continue

--- app/controllers/leagues.controller.js
+++ app/controllers/leagues.controller.js
@@ -2,7 +2,8 @@ import BaseController from './base.controller';
 import LeagueModel from '../models/league';
 
 class LeaguesController extends BaseController {
-  listAll = async (req, res, next) => {
+  // List all leagues
+  list = async (req, res, next) => {
     try {
       res.json(await LeagueModel.find());
     } catch(err) {
@@ -10,27 +11,35 @@ class LeaguesController extends BaseController {
     }
   }
 
+  // Create a league
   create = async (req, res, next) => {
   }
 
+  // Get a specific league info
   get = async (req, res, next, leagueId) => {
   }
 
+  // Update or start/stop a league
   update = async (req, res, next, leagueId) => {
   }
 
+  // User register
   registerPlayer = async (req, res, next, leagueId) => {
   }
 
+  // List all registered players
   listRegisteredPlayers = async (req, res, next, leagueId) => {
   }
 
+  // Add a player to take part in a league
   addPlayer = async (req, res, next, leagueId) => {
   }
 
+  // List all players
   listPlayers = async (req, res, next, leagueId) => {
   }
 
+  // Remove a player
   removePlayer = async (req, res, next, leagueId) => {
   }
 }
--- app/controllers/matches.controller.js
+++ app/controllers/matches.controller.js
@@ -1,20 +1,24 @@
 import BaseController from './base.controller';
 import LeagueModel from '../models/league';
-import MatchModel from '../models/league';
+import MatchModel from '../models/match';
 
 class MatchesController extends BaseController {
+  // List all matches in a league
   listAll = async (req, res, next, leagueId) => {
     try {
-      res.json(await LeagueModel.find());
+      res.json(await MatchModel.find());
     } catch(err) {
       next(err);
     }
   }
 
+  // Get a specific match
   get = async (req, res, next, leagueId, matchId) => {
   }
 
-  
+  // Claim or confirm a match result
+  update = async (req, res, next, leagueId, matchId) => {
+  }
 }
 
 export default new LeaguesController();
